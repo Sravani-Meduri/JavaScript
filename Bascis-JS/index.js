e182907595530e99bcbb917c54bc6f136c26ea6e
Sravani <sravanimeduri03@gmail.com>
2025-05-15 00:40:43 +0530

    Changed Primitive datatype

--- Bascis-JS/index.js
+++ Bascis-JS/index.js
@@ -1,2 +1,5 @@
-console.log("Welcome to JavaScript Tutorial")
-document.write("Welcome to JavaScript Tutorial")
+//console.log("Welcome to JavaScript Tutorial")//
+//document.write("Welcome to JavaScript Tutorial")//
+
+let n=10
+console.log(n)
