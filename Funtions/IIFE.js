92c74d156acf0e356c41ce8511baa7de862381ac
Sravani <sravanimeduri03@gmail.com>
2025-05-29 23:25:42 +0530

    /notes update (29-05-2025)

--- Funtions/IIFE.js
+++ Funtions/IIFE.js
@@ -1,50 +1,77 @@
-//simple IIFE function without parameters
+// //simple IIFE function without parameters
 
-(
-    function(){
-        let a = 1
-        console.log(`JavaScript is no.${a} language`);
-        console.log(45);
-    }
-)();
+// (
+//     function(){
+//         let a = 1
+//         console.log(`JavaScript is no.${a} language`);
+//         console.log(45);
+//     }
+// )();
 
-//IIFE using parameters
+// //IIFE using parameters
 
-(
-    function(MovieName, HeroName, Collection){
-        //console.log("MovieName:", MovieName+ " "+ "HeroName:", HeroName+" "+ "Collection:", Collection);
-        console.log("MovieName:" +MovieName+ " "+ "HeroName:" +HeroName+" "+ "Collection:" + Collection);
+// (
+//     function(MovieName, HeroName, Collection){
+//         //console.log("MovieName:", MovieName+ " "+ "HeroName:", HeroName+" "+ "Collection:", Collection);
+//         console.log("MovieName:" +MovieName+ " "+ "HeroName:" +HeroName+" "+ "Collection:" + Collection);
 
-    }
-)("Pushpa", "AlluArjun", '1500000');
+//     }
+// )("Pushpa", "AlluArjun", '1500000');
 
-//callback functions without parameters - In callback function, we pass the argument type as function
+// //callback functions without parameters - In callback function, we pass the argument type as function
 
-function StudentDetails(callbackfn){
-    callbackfn();
+// function StudentDetails(callbackfn){
+//     callbackfn();
+
+// }
+// StudentDetails(function(){
+//     console.log("Sravani");
+// });
+
+// //callback function with parameters
+
+// function playerDetails(callbackfn){
+//     callbackfn("Virat Kohli", "Sachin Tendulkar")
+
+// }
+// playerDetails(function(Player1,Player2){
+//     console.log("MS DHONI");
+//     console.log(Player1);
+//     console.log(Player2);
+// })
+
+// //using predefined functions in anonymous functions
+
+// //predefined function used here is setTimeout()  ---> it prints the o/p after specific time
+
+// setTimeout(function(){
+//     console.log("Sravani");
+// },3000);
+
+
+
+function getTeamDetails(callbackfn){
+    
+      callbackfn("Dhoni", "captain");
+      
+
 
 }
-StudentDetails(function(){
-    console.log("Sravani");
-});
+  
+getTeamDetails(function(PlayerName, position){
+        console.log("Sravani")
+        console.log(PlayerName);
+      console.log(position);
+    })
 
-//callback function with parameters
+    //getTeamDetails("Dhoni")
+    
 
-function playerDetails(callbackfn){
-    callbackfn("Virat Kohli", "Sachin Tendulkar")
 
-}
-playerDetails(function(Player1,Player2){
-    console.log("MS DHONI");
-    console.log(Player1);
-    console.log(Player2);
-})
+//primitive datatypes - number, string, boolean, undefined, null
 
-//using predefined functions in anonymous functions
 
-//predefined function used here is setTimeout()  ---> it prints the o/p after specific time
 
-setTimeout(function(){
-    console.log("Sravani");
-},3000);
+
+
 
